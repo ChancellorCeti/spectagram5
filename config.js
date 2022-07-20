972d48c5d05be4a7faf292d7eff38599f70b7832
ChancellorCeti <74802589+ChancellorCeti@users.noreply.github.com>
2022-07-20 18:43:39 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAKMMOQh4IO_4_fFPS1cBQ9Zda-jQyW5-U",
+    authDomain: "spectagram-a6e55.firebaseapp.com",
+    projectId: "spectagram-a6e55",
+    storageBucket: "spectagram-a6e55.appspot.com",
+    messagingSenderId: "544089769063",
+    appId: "1:544089769063:web:660ac0bb99348cbd0400c2"
+  };
